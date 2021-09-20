4e09e39007ec004a1034b2a7caeb407232571979
Tpt <thomaspt@hotmail.fr>
2021-09-20 21:59:32 +0200

    Simplifies Method implementation

--- src/model/method.rs
+++ src/model/method.rs
@@ -1,4 +1,4 @@
-use std::borrow::Borrow;
+use std::borrow::{Borrow, Cow};
 use std::convert::{TryFrom, TryInto};
 use std::error::Error;
 use std::fmt;
@@ -15,67 +15,44 @@ use std::str::FromStr;
 /// # Result::<_,Box<dyn std::error::Error>>::Ok(())
 /// ```
 #[derive(PartialEq, Eq, Debug, Clone, Hash)]
-pub struct Method(MethodAlt);
+pub struct Method(Cow<'static, str>);
 
 impl Method {
     /// [CONNECT](https://httpwg.org/http-core/draft-ietf-httpbis-semantics-latest.html#CONNECT).
-    pub const CONNECT: Method = Self(MethodAlt::Connect);
+    pub const CONNECT: Method = Self(Cow::Borrowed("CONNECT"));
     /// [DELETE](https://httpwg.org/http-core/draft-ietf-httpbis-semantics-latest.html#DELETE).
-    pub const DELETE: Method = Self(MethodAlt::Delete);
+    pub const DELETE: Method = Self(Cow::Borrowed("DELETE"));
     /// [GET](https://httpwg.org/http-core/draft-ietf-httpbis-semantics-latest.html#GET).
-    pub const GET: Method = Self(MethodAlt::Get);
+    pub const GET: Method = Self(Cow::Borrowed("GET"));
     /// [HEAD](https://httpwg.org/http-core/draft-ietf-httpbis-semantics-latest.html#HEAD).
-    pub const HEAD: Method = Self(MethodAlt::Head);
+    pub const HEAD: Method = Self(Cow::Borrowed("HEAD"));
     /// [OPTIONS](https://httpwg.org/http-core/draft-ietf-httpbis-semantics-latest.html#OPTIONS).
-    pub const OPTIONS: Method = Self(MethodAlt::Options);
+    pub const OPTIONS: Method = Self(Cow::Borrowed("OPTIONS"));
     /// [POST](https://httpwg.org/http-core/draft-ietf-httpbis-semantics-latest.html#POST).
-    pub const POST: Method = Self(MethodAlt::Post);
+    pub const POST: Method = Self(Cow::Borrowed("POST"));
     /// [PUT](https://httpwg.org/http-core/draft-ietf-httpbis-semantics-latest.html#PUT).
-    pub const PUT: Method = Self(MethodAlt::Put);
+    pub const PUT: Method = Self(Cow::Borrowed("PUT"));
     /// [TRACE](https://httpwg.org/http-core/draft-ietf-httpbis-semantics-latest.html#TRACE).
-    pub const TRACE: Method = Self(MethodAlt::Trace);
-}
-
-#[derive(PartialEq, Eq, Debug, Clone, Hash)]
-enum MethodAlt {
-    Connect,
-    Delete,
-    Get,
-    Head,
-    Options,
-    Post,
-    Put,
-    Trace,
-    Other(String),
+    pub const TRACE: Method = Self(Cow::Borrowed("TRACE"));
 }
 
 impl Deref for Method {
     type Target = str;
 
     fn deref(&self) -> &str {
-        match &self.0 {
-            MethodAlt::Connect => "CONNECT",
-            MethodAlt::Delete => "DELETE",
-            MethodAlt::Get => "GET",
-            MethodAlt::Head => "HEAD",
-            MethodAlt::Options => "OPTIONS",
-            MethodAlt::Post => "POST",
-            MethodAlt::Put => "PUT",
-            MethodAlt::Trace => "TRACE",
-            MethodAlt::Other(method) => method,
-        }
+        &self.0
     }
 }
 
 impl AsRef<str> for Method {
     fn as_ref(&self) -> &str {
-        self.deref()
+        &self.0
     }
 }
 
 impl Borrow<str> for Method {
     fn borrow(&self) -> &str {
-        self.deref()
+        &self.0
     }
 }
 
@@ -83,25 +60,12 @@ impl FromStr for Method {
     type Err = InvalidMethod;
 
     fn from_str(name: &str) -> Result<Self, InvalidMethod> {
-        if name.eq_ignore_ascii_case("CONNECT") {
-            Ok(Self(MethodAlt::Connect))
-        } else if name.eq_ignore_ascii_case("DELETE") {
-            Ok(Self(MethodAlt::Delete))
-        } else if name.eq_ignore_ascii_case("GET") {
-            Ok(Self(MethodAlt::Get))
-        } else if name.eq_ignore_ascii_case("HEAD") {
-            Ok(Self(MethodAlt::Head))
-        } else if name.eq_ignore_ascii_case("OPTIONS") {
-            Ok(Self(MethodAlt::Options))
-        } else if name.eq_ignore_ascii_case("POST") {
-            Ok(Self(MethodAlt::Post))
-        } else if name.eq_ignore_ascii_case("PUT") {
-            Ok(Self(MethodAlt::Put))
-        } else if name.eq_ignore_ascii_case("TRACE") {
-            Ok(Self(MethodAlt::Trace))
-        } else {
-            name.to_owned().try_into()
+        for method in STATIC_METHODS {
+            if method.eq_ignore_ascii_case(name) {
+                return Ok(method);
+            }
         }
+        name.to_owned().try_into()
     }
 }
 
@@ -109,23 +73,12 @@ impl TryFrom<String> for Method {
     type Error = InvalidMethod;
 
     fn try_from(name: String) -> Result<Self, InvalidMethod> {
-        if name.eq_ignore_ascii_case("CONNECT") {
-            Ok(Self(MethodAlt::Connect))
-        } else if name.eq_ignore_ascii_case("DELETE") {
-            Ok(Self(MethodAlt::Delete))
-        } else if name.eq_ignore_ascii_case("GET") {
-            Ok(Self(MethodAlt::Get))
-        } else if name.eq_ignore_ascii_case("HEAD") {
-            Ok(Self(MethodAlt::Head))
-        } else if name.eq_ignore_ascii_case("OPTIONS") {
-            Ok(Self(MethodAlt::Options))
-        } else if name.eq_ignore_ascii_case("POST") {
-            Ok(Self(MethodAlt::Post))
-        } else if name.eq_ignore_ascii_case("PUT") {
-            Ok(Self(MethodAlt::Put))
-        } else if name.eq_ignore_ascii_case("TRACE") {
-            Ok(Self(MethodAlt::Trace))
-        } else if name.is_empty() {
+        for method in STATIC_METHODS {
+            if method.eq_ignore_ascii_case(&name) {
+                return Ok(method);
+            }
+        }
+        if name.is_empty() {
             Err(InvalidMethod(InvalidMethodAlt::Empty))
         } else {
             for c in name.chars() {
@@ -139,7 +92,7 @@ impl TryFrom<String> for Method {
                     }));
                 }
             }
-            Ok(Self(MethodAlt::Other(name)))
+            Ok(Self(name.into()))
         }
     }
 }
@@ -149,6 +102,17 @@ impl fmt::Display for Method {
         write!(f, "{}", self.as_ref())
     }
 }
+
+const STATIC_METHODS: [Method; 8] = [
+    Method::CONNECT,
+    Method::DELETE,
+    Method::GET,
+    Method::HEAD,
+    Method::OPTIONS,
+    Method::POST,
+    Method::PUT,
+    Method::TRACE,
+];
 
 /// Error returned by [`Method::try_from`].
 #[derive(Debug, Clone)]
